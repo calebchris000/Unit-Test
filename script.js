0104b0967156a601c135f48e34d8028392087628
calebchris000 <calebchris000@gmail.com>
2023-02-28 08:12:33 +0100

    Unit test scripts

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
+// * Return the string length
+const stringLength = (str) =>
+  str.length <= 0 ? "Sorry, but put in something" : str.length;
+
+//* Reverses a string.
+const reverseString = (string) => string.split("").reverse().join("");
+
+// * A simple calculator app
+class Calculator {
+  add(a, b) {
+    return eval(a + b);
+  }
+
+  subtract(a, b) {
+    return eval(a - b);
+  }
+
+  multiply(a, b) {
+    return eval(a * b);
+  }
+
+  divide(a, b) {
+    return eval(a / b);
+  }
+}
+
+const capitalize = (string) => {
+  let split = `${string.charAt(0).toUpperCase() + string.slice(1)}`;
+ return split
+};
 
 
-const stringLength = (str) => str.length <=0 ? 'Sorry, but put in something': str.length;
-
-module.exports = stringLength;
+module.exports = {
+  stringLength: stringLength,
+  reverseString: reverseString,
+  Calculator: Calculator,
+  capitalize: capitalize
+};
--- script.test.js
+++ script.test.js
@@ -1,7 +1,78 @@
-const stringLength = require('./script.js');
+const { stringLength, reverseString, Calculator, capitalize } = require("./script.js");
+const word = "hello";
+const another = "hello";
 
-const word = 'hello'
+describe("String", () => {
+  test("Return string length ", () => {
+    expect(stringLength(word)).toBe(word.length);
+  });
 
-test('Return string length ', () => {
-    expect(stringLength(word)).toBe(word.length)
+  test("Reverses string", () => {
+    expect(reverseString(another)).toBe("olleh");
+  });
 });
+
+let myCalc = new Calculator();
+
+describe("Add number", () => {
+  test("Add number group", () => {
+    expect(myCalc.add(2, 3)).toBe(5);
+  });
+
+  test("Add num", () => {
+    expect(myCalc.add(34, 7)).toBe(34 + 7);
+  });
+
+  test("Add num", () => {
+    expect(myCalc.add(123, 371)).toBe(123 + 371);
+  });
+});
+
+describe("Subtract number group", () => {
+  test("Subtract 82 - 5", () => {
+    expect(myCalc.subtract(82, 5)).toBe(82 - 5);
+  });
+
+  test("Subtract 34 - 7", () => {
+    expect(myCalc.subtract(34, 7)).toBe(34 - 7);
+  });
+
+  test("Subtract 1022 - 371", () => {
+    expect(myCalc.subtract(1022, 371)).toBe(1022 - 371);
+  });
+});
+
+describe("Multiply number group", () => {
+  test("Multiply 3 * 4", () => {
+    expect(myCalc.multiply(3, 4)).toBe(3 * 4);
+  });
+
+  test("Multiply 12 * 7", () => {
+    expect(myCalc.multiply(12, 7)).toBe(12 * 7);
+  });
+
+  test("Multiply 121 * 12", () => {
+    expect(myCalc.multiply(121, 12)).toBe(121 * 12);
+  });
+});
+
+describe("Divide number group", () => {
+    test("Divide 9 / 3", () => {
+      expect(myCalc.divide(9, 3)).toBe(9 / 3);
+    });
+  
+    test("Divide 36 */ 6", () => {
+      expect(myCalc.divide(36, 6)).toBe(36 / 6);
+    });
+  
+    test("Divide 65536 / 16", () => {
+      expect(myCalc.divide(65536, 16)).toBe(65536 / 16);
+    });
+  });
+
+
+  describe('Check for Capitalize String', () => {
+    test('capitalize', () => {
+        expect(capitalize('hello')).toBe('Hello')
+    })
+  })
